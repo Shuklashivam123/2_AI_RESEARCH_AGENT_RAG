e09f5854f80276d00e7478aad04e248cd300729b
Shivam Shukla <shuklashivam100803@gmail.com>
2026-02-09 17:28:04 +0530

    1 Completed entire structure of the Project

---
+++ template.sh
@@ -0,0 +1,17 @@
+mkdir data
+mkdir templates
+mkdir research
+mkdir src
+mkdir static
+mkdir app.py
+
+
+touch README.md
+touch .gitignore
+touch src/helper.py
+touch requirements.txt
+touch src/__init__.py
+touch templates/index.html
+touch research/trials.py
+
+echo "Project structure created successfully!"
